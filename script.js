5ae846ef517f9921a8b9333afff27eccc391ec02
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-04-04 02:19:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-
-let board = [];
-
-function initializeBoard() {
-    for(let i = 0;i < 25;i++) {
-        board[i] = [];
-        for(let j = 0;j < 25;j++) {
-            board[i][j] = 0;
+var grid = []
+var columns = 5
+var rows = 5
+var container = []
+function setContainer() {
+    container = document.createElement("div")
+    container.classList.add("container")
+    document.body.append(container)
+    for(let i = 0;i < columns;i++) {
+        grid[i] = []
+        for(let j = 0;j < rows;j++) {
+            grid[i][j] = 0
         }
     }
 }
-let gridContainer = document.createElement("div");
-function drawBoard() {
-    document.body.appendChild(gridContainer);
-    for(let i = 0;i < 25;i++)
-        {for(let j = 0;j < 25;j++) {
-            let gridItem = document.createElement("div");
-            gridContainer.appendChild(gridItem);
-
+function drawGrid() {
+    for(let i = 0;i < columns;i++) {
+        for(let j = 0;j < rows;j++) {
+            var Node = document.createElement("div")
+            Node.classList.add("Node")
+            container.appendChild(Node)
         }
     }
 }
-console.log(board)
+
+function Node(i, j) {
+    var h = (Math.sqrt(i - (columns- 1))*(i - (columns - 1)+(j -(rows - 1)*(j- (rows - 1)))))
+    var g = 0
+    var f = g + h
+}
+
+function surronding(current) {
+    let temp = []
+    for(let i = 0;i < 2;i++) {
+        temp.push(current[i][+1])
+    }
+    temp.push(current[-1])
+    temp.push(current[+1])
+    for(let j = 0;j < 2;j++) {
+        temp.push(current[i][-i])
+    }
+    return temp
+}
+
+function aStar() {
+    var openSet = grid[0][0]
+    
+
+    while(openSet != 0) {
+        var current = Math.min(Node(openSet))
+        if(current == end) {
+            break
+        } else {
+            var temp = surronding(current)
+            console.log(temp)
+        }
+    }
+}
