476117ac8c3485d9cfb002f15c4a8037be270cf0
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-04-05 20:12:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,55 +4,47 @@ var grid = []
 
 
 function Nodes(x, y, container) {
-    var h = (Math.sqrt(x - (columns - 1))*(x - (columns - 1))+(y - (rows - 1)+(y - (rows - 1))))
-    var g = 0 // start at 0,0 so g can be 0
+    var h = Math.sqrt((x - (columns - 1)) * (x - (columns - 1)) + (y - (rows - 1)) * (y - (rows - 1)));
+    var g = 0
     var f = g + h
     var status = 0
-    var div = document.createElement("div")
-    div.classList.add("node")
-    container.appendChild(div)
-    return div;
+    var node = document.createElement("div")
+    node.classList.add("Node")
+    container.appendChild(node)
+    return node
 }
 function setGrid() {
+    var div = document.createElement("div")
+    div.classList.add("container")
+    document.body.append(div)
     for(let i = 0;i < columns;i++) {
         grid[i] = []
         for(let j = 0;j < rows;j++) {
-            grid[i][j] = Nodes(i, j)
+            grid[i][j] = Nodes(i, j, div)
         }
     }
 }
-function surrounding() {
-    let temp = []
-    for(let i = 0;i < 2;i++) {
-        temp.push(current[i][+1])
+
+function queue(openSet) {
+    var collection = []
+    for(let i = 0;i < 3;i++) {
+        
     }
-    temp.push(current[-1])
-    temp.push(current[+1])
-    for(let j = 0;j < 2;j++) {
-        temp.push(current[i][-i])
-    }
-    return temp
+    
+
 }
-function aStar() {
-    var start = grid[0][0]
+
+
+function aStar(start) {
     var openSet = []
     openSet.push(start)
-    var closedSet = []
-    var result = grid[rows - 1][columns - 1]
-    while(openSet != 0) {
-    //or current Node and current node does not equal goal
-    var current = openset[0][0]
-    if(current != 0) {
+    
+    
 
-        } else {
-            var surrounding = []
-            surrounding = surrounding(current)
-        }
-    }
 }
+    
 function main() {
-    var container = document.createElement("div")
-    document.body.append(container)
     setGrid()
-    aStar()
+    var start = grid[0][0]
+    aStar(start)
 }
