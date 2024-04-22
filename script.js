2079b2a675844f47c8b3aa995d898018ff5f7375
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-04-21 23:38:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,68 @@
-var columns = 5
-var rows = 5
-var grid = []
-
-
-function Nodes(x, y, container) {
-    var h = Math.sqrt((x - (columns - 1)) * (x - (columns - 1)) + (y - (rows - 1)) * (y - (rows - 1)));
-    var g = 0
-    var f = g + h
-    var status = 0
-    var node = document.createElement("div")
-    node.classList.add("Node")
-    container.appendChild(node)
-    return node
-}
-function setGrid() {
-    var div = document.createElement("div")
-    div.classList.add("container")
-    document.body.append(div)
-    for(let i = 0;i < columns;i++) {
-        grid[i] = []
-        for(let j = 0;j < rows;j++) {
-            grid[i][j] = Nodes(i, j, div)
-        }
+board = []
+rows = 5
+columns = 5
+for(let x = 0;x < rows;x++) {
+    row = []
+    for(let y = 0;y < columns;y++) {
+        row.push(0)
     }
+    board.push(row)
 }
+startX = 1
+startY = 1
+endX = 4
+endY = 4
+openSet = []
+closedSet = []
 
-function queue(openSet) {
-    var collection = []
-    for(let i = 0;i < 3;i++) {
+function astar(startX, startY, endX, endY) {
+    openSet.push([startX, startY])
+    while(openSet.length > 0) {
+        let curr = lowestScore(openSet)
+        console.log(curr)
+        if(curr[0] == endX && curr[1] == endY) {
+            return 0
+        }
+        closedSet.push(curr)
+        openSet = openSet.filter(node => !(node[0] === curr[0] && node[1] === curr[1]));
+        console.log(openSet)
+        checkNeighbours(curr)
+        
+        let [tempX, tempY] = lowestScore(openSet)
+        console.log(tempX, tempY)
         
     }
-    
+    return("not working")
 
 }
-
-
-function aStar(start) {
-    var openSet = []
-    openSet.push(start)
-    
-    
-
+function checkNeighbours(curr){
+    let [x, y] = curr
+    var neighbours = []
+    if (board[x + 1] !== undefined && board[x + 1][y] !== undefined) neighbours.push([x + 1, y]);
+    if (board[x + 1] !== undefined && board[x + 1][y + 1] !== undefined) neighbours.push([x + 1, y + 1]);
+    if (board[x + 1] !== undefined && board[x + 1][y - 1] !== undefined) neighbours.push([x + 1, y - 1]);
+    if (board[x - 1] !== undefined && board[x - 1][y - 1] !== undefined) neighbours.push([x - 1, y - 1]);
+    if (board[x - 1] !== undefined && board[x - 1][y] !== undefined) neighbours.push([x - 1, y]);
+    if (board[x - 1] !== undefined && board[x - 1][y + 1] !== undefined) neighbours.push([x - 1, y + 1]);
+    if (board[x][y - 1] !== undefined) neighbours.push([x, y - 1]);
+    if (board[x][y + 1] !== undefined) neighbours.push([x, y + 1]);
+    neighbours.forEach(neighbours => {
+        openSet.push(neighbours)
+    })
+    return openSet
 }
-    
-function main() {
-    setGrid()
-    var start = grid[0][0]
-    aStar(start)
+function lowestScore(openSet) {
+    for(let i = 0;i < openSet.length;i++) {
+        let [x, y] = openSet[i]
+        let h = Math.sqrt((endX - x)**2 + (endY - y)**2)
+        let g = Math.sqrt((x - endX)**2 + (y - endY)**2)
+        let f = h + g
+        var lowest = Math.min(lowest, f)
+        lowestX = x
+        lowestY = y
+    }
+    return [lowestX, lowestY]
 }
+
+console.log(board)
+astar(startX, startY, endX, endY)
