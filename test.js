96b684ad05885f3f0dda84d313018fcad837d137
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-04-24 17:57:31 -0400

    Update test.js

--- test.js
+++ test.js
@@ -8,47 +8,60 @@ for(let x = 0;x < rows;x++) {
     }
     board.push(row)
 }
-startX = 0
-startY = 0
-endX = 4
-endY = 4
+start = [1, 1]
+end = [4, 4]
 openSet = []
 closedSet = []
-function astar(startX, startY, endX, endY) {
-    openSet.push([startX, startY])
-
-    while(openSet > 0) {
-        let curr = lowestScore(openSet)
-        console.log(curr)
-        if(curr[0] == endX && curr[1] == endY) {
+function astar(start, end) {
+    openSet.push(start)
+    while(openSet.length > 0) {
+        curr = lowestScore(openSet,start, end) 
+        if(curr == end) {
+            console.log("end condition met")
             return 0
         }
-        openSet.splice(current)
-        neighbours = neighbours(current)
-        for(let i = 0;i < neighbours.length;i++) {
-            temp = lowestScore(neighbours[i])
-            next = Math.min(temp, next)
-            if(neighbours.indexOf(openSet)) {
-                openSet.push(neighbours)
-            }
+        openSet.splice(openSet.indexOf(curr))
+        closedSet.push(curr)
+        openSet.push(neighbours(curr))
+        
+        
 
+    }
+}
+function lowestScore(openSet, start, end) {
+    //function needs to be able to take in values in the same array like format as start and end
+    lowest = 1000
+    startX = start[0]
+    startY = start[1]
+    endX = end[0]
+    endY = end[1]
+    for(let i = 0;i < openSet.length;i++) {
+        let [x, y] = openSet[i]  
+        let g = Math.sqrt((startX - x)**2 + (startY - y)**2)
+        let h = Math.sqrt((endX - x)**2 + (endY - y)**2)
+        let f = h + g
+        if(f < lowest) {    
+        lowest = f
+        curr = openSet[i]
         }
     }
-    return("not working")
+    return curr
+}
+function neighbours(curr) {
+    let [x, y] = curr
+    var neighbours = []
+    // needs to be not undefined and needs to not exist in closedSet and make sure that the board value is equal to 0(not obsticle)
+    if(board[x + 1][y] != undefined && closedSet.includes(board[x + 1][y]) == false) { neighbours.push([x + 1, y])}
+    if(board[x + 1][y + 1] != undefined && closedSet.includes(board[x + 1][y + 1]) == false) {neighbours.push([x + 1][y + 1])}
+    if(board[x][y + 1] != undefined && closedSet.includes(board[x][y + 1]) == false) { neighbours.push([x, y + 1])}
+    if(board[x + 1][y - 1] != undefined && closedSet.includes(board[x + 1][y - 1]) == false) { neighbours.push([x + 1, y - 1])}
+    if(board[x - 1][y] != undefined && closedSet.includes(board[x - 1][y]) == false) { neighbours.push([x - 1, y])}
+    if(board[x - 1][y - 1] != undefined && closedSet.includes(board[x - 1][y - 1]) == false) { neighbours.push([x - 1, y - 1])}
+    if(board[x][y - 1] != undefined && closedSet.includes(board[x][y - 1]) == false) { neighbours.push([x, y - 1])}
 
+
+
+    return neighbours
 }
-function neighbours(board, start) {
-    
-    return
-}
-function lowestScore(openSet) {
-    for(let i = 0;i < openSet.length;i++) {
-        let [x, y] = openSet[i]
-        let h = Math.sqrt((endX - x)*2 + (endY - y)*2)
-        let g = Math.sqrt((x - endX)*2 + (y - endY)*2)
-        let f = h + g
-        lowest = Math.min(lowest, f)
-    }
-    return lowest
-}
-astar(startX, startY, endX, endY)
+
+astar(start ,end)       
